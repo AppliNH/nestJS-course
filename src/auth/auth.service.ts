e751395b891e0abb4a35a548462ab09e41a79db0
AppliNH <thomas.martindev@gmail.com>
2020-09-05 20:48:35 +0200

    simple signup feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,8 @@ export class AuthService {
         private userRepository: UserRepository
     ) {}
 
+    async signUp(authCredentials: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.signUp(authCredentials);
+    }
 
 }
